583b2e7b6cee449495bf7a3a0b063ac65fa3dad4
mohammad-ayan-008 <mohammadayanafaq@gmail.com>
2024-11-10 17:16:49 +0530

    updated --added Validations

--- src/main/java/org/mainfest/devSquare/DevSqaure/controller/UserRegisterationController.java
+++ src/main/java/org/mainfest/devSquare/DevSqaure/controller/UserRegisterationController.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.Map;
 
 @RestController
