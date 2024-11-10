bef210c1f7ecf002c9234f645a8d787b2afd7fef
mohammad-ayan-008 <mohammadayanafaq@gmail.com>
2024-11-10 17:30:33 +0530

    updated --added Validations

--- src/main/java/org/mainfest/devSquare/DevSqaure/controller/UserRegisterationController.java
+++ src/main/java/org/mainfest/devSquare/DevSqaure/controller/UserRegisterationController.java
@@ -64,7 +64,6 @@ public class UserRegisterationController {
     }
 
 
-
     @GetMapping("/invalidate")
     public ResponseEntity<?> invalidate(){
         return ResponseEntity.ok(Map.of("status",""+userService.invalidate()));
